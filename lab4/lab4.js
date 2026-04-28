4e63c906308190e626016dfc94290efe28c5cda4
LukaninaAD <lukaninaanastasia7@gmail.com>
2026-04-28 12:07:19 +0700

    как-то работает

---
+++ lab4/lab4.js
@@ -0,0 +1,45 @@
+/*1*/
+// let name;
+// let admin;
+// name = "Джон";
+// admin = name
+// alert(admin)
+
+// /*2*/
+// let a = prompt("Первое число?", 1);
+// let b = prompt("Второе число?", 2);
+
+// alert(Number(a) + Number(b));
+
+// /*3*/
+// for (let i = 2; i < 11; i=i+2) {
+//     alert(i);
+// }
+
+/*4*/
+// let i = 0;
+// while(i < 3) {
+//     alert( `number ${i}!` );
+//     i++;
+// }
+
+// /*5*/
+// let chislo = prompt("Пожалуйста введите число больше 100");
+// while(chislo <= 100) {
+//     chislo = prompt("Неверно, пожалуйста введите число больше 100");
+// }
+
+// /*6*/
+// let n = prompt("Пожалуйста введите n");
+// for (let i = 2; i <= n; i++) {
+//     let isPrime = true;
+//     for (let j = 2; j * j <= i; j++) {
+//         if (i % j === 0) {
+//             isPrime = false;
+//             break;
+//         }
+//     }
+//     if (isPrime) {
+//         alert(i);
+//     }
+// }
